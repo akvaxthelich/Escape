ac14b9ede5cd4a5289aae3f6cc5ff546da7dbadc
Andrew Dinspechin <58988010+akvaxthelich@users.noreply.github.com>
2022-09-10 02:29:13 -0400

    added first person camera, movement, level building prefabs, textures, materials, menu music, changed colors

---
+++ Assets/Scripts/FPSCamera.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : MonoBehaviour
+{
+    public GameObject cc; //ref character controller for rotation about y axis
+
+    float _senseX = 400;
+    float _senseY = 400; //default sense to 100 for both. may change later
+
+    float xRotation;
+    float yRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //center mousepos
+        Cursor.visible = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * _senseX; //keep smoothing filter for that extra bit of polish
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * _senseY; 
+        
+        yRotation += mouseX; 
+        xRotation -= mouseY;
+
+        //clamp so we dont go super mario galaxy mode:
+        xRotation = Mathf.Clamp(xRotation, -90f, 90); //Remember, this is rotation ABOUT the X axis, not along it.
+        //like walking into a pole at waist height
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        
+        cc.transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovementShite.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementShite : MonoBehaviour
+{
+
+    CharacterController _cc;
+    float _speed = 6f;
+    float _gravity = Physics.gravity.y;
+
+    public float jumpHeight;
+    bool _onGround;
+    float _maxDist = 0.75f;
+    float _sphRadius = 0.5f;
+    int _groundLayerMask;
+
+
+    Vector3 moveInput;
+    Vector3 _velocity;
+
+    void Start()
+    {
+        
+        _cc = GetComponent<CharacterController>();
+        _velocity = Vector3.zero;
+        _groundLayerMask = LayerMask.GetMask("Ground");
+    }
+
+    void Update()
+    {
+        
+        _onGround = Physics.SphereCast(transform.position, _sphRadius, -transform.up, out RaycastHit hit, _maxDist + 0.01f, _groundLayerMask);
+
+
+        #region Mario physics
+        //character should spend half the time going down as they do going up
+
+        if (Input.GetButtonDown("Jump") && _onGround)
+        {
+            //v = sqrt of jumpheight * -2 * gravity (as long as gravity is negative) 
+            _velocity.y = Mathf.Sqrt(jumpHeight * -2 * _gravity);
+        }
+        if (_velocity.y < 0f) //if moving down
+        {
+
+            _gravity = Physics.gravity.y * 2f;
+
+        }
+        else { 
+        
+            _gravity = Physics.gravity.y;
+        }
+
+
+        #endregion
+
+        #region Ground-based Movement
+
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        Vector3 moveDir = transform.forward * moveInput.z + transform.right * moveInput.x;
+
+        //Move on x and z
+        _cc.Move(moveDir * _speed * Time.deltaTime);
+
+        #endregion
+
+        #region Air and Vertical movement (jumping + gravity)
+        if (!_onGround)
+        {
+
+            _velocity.y += _gravity * (Time.deltaTime); // m/s ^2 
+
+        }
+        if (_velocity.y < 0 && _onGround) {
+
+            _velocity.y = 0f;
+
+        }
+        
+        _cc.Move(_velocity * Time.deltaTime);
+        #endregion
+
+    }
+
+
+}
